5b2e12d7e4719488d495e15733034f04fd424183
HP <HP@DESKTOP-D3AR52B>
2020-01-31 12:36:28 +0530

    commit

---
+++ src/test/java/com/maven/scripts/TestJenkins.java
@@ -0,0 +1,14 @@
+package com.maven.scripts;
+
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+
+	
+   @Test
+   public void test1()
+    {
+	System.out.println("========build=====");
+    }
+
+}
